d28f528cb58785c84e443ff4fdc8b245349ef9fd
Hely Saul Oberto <hely.oberto@gmail.com>
2026-03-14 00:05:39 -0400

    cambio en venue para el mapa

--- components/Venue.tsx
+++ components/Venue.tsx
@@ -41,7 +41,7 @@ export default function Venue() {
             <div style={{ padding: "6px", border: "1px solid var(--gold-line)", borderRadius: "1px" }}>
               <div style={{ overflow: "hidden", borderRadius: "1px" }}>
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3921.!2d-69.5855451!3d11.3661906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e842bfeee5f598d%3A0xf403a1c864647f4f!2sCentro%20Recreacional%20Brisas%20del%20Renacer!5e0!3m2!1ses!2sve!4v1&maptype=satellite"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4441.260218991101!2d-69.58554512441269!3d11.36618538882068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e842bfeee5f598d%3A0xf403a1c864647f4f!2sCentro%20Recreacional%20Brisas%20del%20Renacer!5e1!3m2!1ses-419!2sve!4v1773461063954!5m2!1ses-419!2sve"
                   width="100%"
                   height="230"
                   style={{ border: 0, display: "block", filter: "saturate(0.85) brightness(1.02)" }}
